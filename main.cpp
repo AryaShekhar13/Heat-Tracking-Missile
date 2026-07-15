90066fba8ff9b5ef9f1f51e2cd53c76c15d94c83
AryaShekhar13 <kneegrowreal831@gmail.com>
2026-07-15 18:10:24 +0530

    Initial Structure

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <spdlog/spdlog.h>
+#include <math.h>
+#include<cmath>
+using namespace std;
+
+int main(){
+    
+}
---
+++ render.cpp
@@ -0,0 +1,11 @@
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include<SFML/Graphics.hpp>
+#include<SFML/System.hpp>
+#include <iostream>
+
+using namespace sf;
+
+void render(){
+
+}
